2ddb03a95710ee059cd5ca631ad2f961fa55a6f6
anntran7 <ann.tran@mrjaffesclass.com>
2015-04-14 11:21:07 -0700

    Update Card.java

--- Activity01/Card.java
+++ Activity01/Card.java
@@ -6,7 +6,6 @@ package Activity01;
  * <code>Card</code> represents a playing card.
  */
 public class Card {
-
     /**
      * String value that holds the suit of the card
      */
@@ -22,8 +21,7 @@ public class Card {
      */
     private int pointValue;
 
-
-   /**
+    /**
      * Creates a new <code>Card</code> instance.
      *
      * @param cardRank  a <code>String</code> value
@@ -34,16 +32,18 @@ public class Card {
      *                  containing the point value of the card
      */
     public Card(String cardRank, String cardSuit, int cardPointValue) {
+        rank = cardRank;
+        suit = cardSuit;
+        pointValue = cardPointValue;
 
     }
-
 
     /**
      * Accesses this <code>Card's</code> suit.
      * @return this <code>Card's</code> suit.
      */
     public String suit() {
-
+        return suit;
     }
 
     /**
@@ -51,24 +51,34 @@ public class Card {
      * @return this <code>Card's</code> rank.
      */
     public String rank() {
-
+        return rank;
     }
 
-   /**
+    /**
      * Accesses this <code>Card's</code> point value.
      * @return this <code>Card's</code> point value.
      */
     public int pointValue() {
-
+        return pointValue;
     }
 
     /** Compare this card with the argument.
      * @param otherCard the other card to compare to this
      * @return true if the rank, suit, and point value of this card
-     *              are equal to those of the argument;
+     *         are equal to those of the argument;
      *         false otherwise.
      */
     public boolean matches(Card otherCard) {
+        if(this.suit.equals(otherCard.suit())&&
+            this.rank.equals(otherCard.rank())&&
+                this.pointValue==otherCard.pointValue())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
 
     }
 
@@ -84,6 +94,6 @@ public class Card {
      */
     @Override
     public String toString() {
-
+       return this.rank + " of " + this.suit + "(point value = " + this.pointValue + " )";
     }
 }
